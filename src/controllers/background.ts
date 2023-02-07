ed67b883ad1b3e4679878d78f087a881845fd10b
Casey K <tehcnicolour@gmail.com>
2023-02-07 09:33:12 -0800

    rm settings service as it will remain on the client

--- src/controllers/background.ts
+++ src/controllers/background.ts
@@ -2,7 +2,6 @@ import WalletService from "./wallet";
 import { AppService } from "../util/svc";
 
 import NodeService from "./node";
-import SettingsService from "./settings";
 // import AnalyticsService from "./analytics";
 
 let appService: AppService;
@@ -10,7 +9,6 @@ let appService: AppService;
 export const initServices = (): AppService => {
   if (!appService) {
     appService = new AppService();
-    appService.add("setting", new SettingsService());
     appService.add("node", new NodeService());
     appService.add("wallet", new WalletService());
     // appService.add("analytics", new AnalyticsService());
